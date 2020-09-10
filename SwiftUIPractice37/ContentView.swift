69f9a80125655eb1a001fe22fcedc63d30e0a108
tanvir <tanvirgeek@gmail.com>
2020-09-10 19:03:33 +0600

    Initial Commit

---
+++ SwiftUIPractice37/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice37
+//
+//  Created by MD Tanvir Alam on 10/9/20.
+//  Copyright © 2020 MD Tanvir Alam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
